03508564d5c071b98e92cbbcd5be5afed1db6c44
Yana Pavlova <yn-pvlv@yandex.ru>
2025-02-11 17:52:59 +0300

    fix: change layout

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
 import { ItemPage } from '../../pages/ItemPage/ItemPage'
-import { HomePage } from '../../pages/HomePage/HomePage'
 import { NotFoundPage } from '../../pages/NotFoundPage/NotFoundPage'
 import { Layout } from '../Layout'
 import { FormPage } from '../../pages/FormPage/FormPage'
@@ -13,7 +12,7 @@ export const App = () => {
       <Layout>
         <EditProvider>
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<ListPage />} />
             <Route path="list" element={<ListPage />} />
             <Route path="item/:id" element={<ItemPage />} />
             <Route path="/form" element={<FormPage />} />
