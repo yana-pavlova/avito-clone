aaa5fe68f687bffa223db554f9b025e900351c1a
Yana Pavlova <yn-pvlv@yandex.ru>
2025-02-11 16:13:55 +0300

    feat: create routing

--- client/src/components/app/App.tsx
+++ client/src/components/app/App.tsx
@@ -1,3 +1,26 @@
+import { Route, Routes } from 'react-router-dom'
+import { ItemPage } from '../../pages/ItemPage/ItemPage'
+import { HomePage } from '../../pages/HomePage/HomePage'
+import { NotFoundPage } from '../../pages/NotFoundPage/NotFoundPage'
+import { Layout } from '../Layout'
+import { FormPage } from '../../pages/FormPage/FormPage'
+import { EditProvider } from '../../store/context'
+import { ListPage } from '../../pages/ListPage/ListPage'
+
 export const App = () => {
-  return <div></div>
+  return (
+    <div>
+      <Layout>
+        <EditProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="list" element={<ListPage />} />
+            <Route path="item/:id" element={<ItemPage />} />
+            <Route path="/form" element={<FormPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </EditProvider>
+      </Layout>
+    </div>
+  )
 }
