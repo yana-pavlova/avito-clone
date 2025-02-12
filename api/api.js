0da56e08df4987d5b5b8e8cda90ee2008d9746cf
Yana Pavlova <yn-pvlv@yandex.ru>
2025-02-12 14:50:30 +0300

    fix: male mileage property be  not required according to the TA

--- api/api.js
+++ api/api.js
@@ -40,7 +40,7 @@ app.post('/items', (req, res) => {
       }
       break
     case ItemTypes.AUTO:
-      if (!rest.brand || !rest.model || !rest.year || !rest.mileage) {
+      if (!rest.brand || !rest.model || !rest.year) {
         return res
           .status(400)
           .json({ error: 'Missing required fields for Auto' })
