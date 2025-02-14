58edcce8a4fbf9c5b3fbef6aa8d61bd5d0264972
Yana Pavlova <yn-pvlv@yandex.ru>
2025-02-14 13:38:55 +0300

    fix: fix incorrect style properties syntax

--- client/src/components/back-button/BackButton.tsx
+++ client/src/components/back-button/BackButton.tsx
@@ -16,9 +16,9 @@ export const BackButton = ({ onClick }: BackButtonProps) => {
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path d="M15 18l-6-6 6-6" />
       </svg>
